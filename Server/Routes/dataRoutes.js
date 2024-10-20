9ce52c286abb22609db0acc32fd83f5f28a88dfe
AbdallahAlqadi <161605945+AbdallahAlqadi@users.noreply.github.com>
2024-10-20 20:33:31 +0300

    add

--- Server/Routes/dataRoutes.js
+++ Server/Routes/dataRoutes.js
@@ -3,11 +3,10 @@ const cors=require('cors');
 const routes=express.Router();
 require('dotenv').config();
 
-const {createData,getData,DeleteData}=require('../controllers/datacontroller'); 
+const {createData,getData}=require('../controllers/datacontroller'); 
 
 routes.post('/data',createData);
 routes.get('/data',getData);
-routes.delete('/data',DeleteData);
 
 
 module.exports=routes;
